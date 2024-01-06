673f5795ebde03b6a1c0d82f66776038fd4a8af3
newrodrigo <rodrigocerqueira@outlook.com>
2024-01-06 16:35:46 -0300

    Search part2

--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,3 +1,4 @@
+import Button from "../../components/Button";
 import Header from "../../components/Header";
 import "./styles.css";
 
@@ -6,8 +7,22 @@ export default function Search() {
     <>
       <Header />
       <main>
-        <section>
-            <h1>Encontre um perfil Github</h1>
+        <section className="user-card-container">
+          <h1>Encontre um perfil Github</h1>
+          <form name="user">
+            <div className="user-form-control">
+              <label htmlFor="userName">Usuário</label>
+              <input
+                name="userName"
+                id="usesrName"
+                type="text"
+                placeholder="Usuário Github"
+              />
+            </div>
+          </form>
+          <div>
+            <Button text="Encontrar" />
+          </div>
         </section>
       </main>
     </>
